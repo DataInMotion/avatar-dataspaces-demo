992063b69f42155f9a1339812df578a0e78a7dfc
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2024-11-22 17:41:12 +0100

    refactored project
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

---
+++ de.avatar.connector.isma/src/de/avatar/connector/isma/ISMAConnectorImpl.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2012 - 2021 Paremus Ltd., Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms of the 
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors:
+ * 		Paremus Ltd. - initial API and implementation
+ *      Data In Motion
+ */
+package de.avatar.connector.isma;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+import de.avatar.connector.api.AvatarConnector;
+import de.avatar.model.connector.AConnectorFactory;
+import de.avatar.model.connector.ConnectorEndpoint;
+import de.avatar.model.connector.ConnectorInfo;
+import de.avatar.model.connector.ConnectorMetric;
+import de.avatar.model.connector.EndpointRequest;
+import de.avatar.model.connector.EndpointResponse;
+import de.avatar.model.connector.JavaParameter;
+import de.avatar.model.connector.StatusType;
+
+@Component(immediate = true, property = {
+		"service.exported.configs=com.paremus.dosgi.net", 
+		"service.exported.interfaces=*", 
+		"com.paremus.dosgi.scope=global", 
+		"com.paremus.dosgi.target.clusters=DIMC", 
+		"com.paremus.dosgi.net.serialization=ecore",
+		"connector=isma.himsa"})
+public class ISMAConnectorImpl implements AvatarConnector {
+	
+	@Reference
+	private AConnectorFactory connectorFactory;
+	private long startTimestamp;
+	
+	@Activate
+	public void activate() {
+		System.out.println("Activate ISMA-Connector-Implementation");
+		startTimestamp = Instant.now().getEpochSecond();
+	}
+	
+	@Deactivate
+	public void deactivate() {
+		System.out.println("De-activate ISMA-Connector-Implementation");
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see de.avatar.connector.api.AvatarConnectorInfo#getEndpoints()
+	 */
+	@Override
+	public List<ConnectorEndpoint> getEndpoints() {
+		List<ConnectorEndpoint> eps = new ArrayList<>();
+		ConnectorEndpoint ep = connectorFactory.createConnectorEndpoint();
+		ep.setUri("mqtt://isma/himsa");
+		ep.setId("mqtt_isma_himsa");
+		ep.setName("ISMA HIMSA MQTT Endpoint");
+		eps.add(ep);
+		ep = connectorFactory.createConnectorEndpoint();
+		ep.setUri("http://isma/himsa/rest");
+		ep.setId("rest_isma_himsa");
+		ep.setName("ISMA HIMSA Rest Endpoint");
+		eps.add(ep);
+		return eps;
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see de.avatar.connector.api.AvatarConnectorInfo#getInfo()
+	 */
+	@Override
+	public ConnectorInfo getInfo() {
+		ConnectorInfo info = connectorFactory.createConnectorInfo();
+		info.setId("isma_himsa");
+		info.setName("ISMA Himsa Connector");
+		info.setVersion((short)1);
+		ConnectorMetric metric = connectorFactory.createConnectorMetric();
+		metric.setStatus(StatusType.RUNNING);
+		metric.setTimestamp(Instant.now().toEpochMilli());
+		metric.setUptime(Instant.now().getEpochSecond() - startTimestamp);
+		info.setMetric(metric);
+		info.getEndpoint().addAll(getEndpoints());
+		return info;
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see de.avatar.connector.api.AvatarConnector#dryRequest(de.avatar.model.connector.EndpointRequest)
+	 */
+	@Override
+	public EndpointResponse dryRequest(EndpointRequest request) {
+		EndpointResponse response = connectorFactory.createEndpointResponse();
+		response.setId(request.getId());
+		response.setRequest(EcoreUtil.copy(request));
+		response.setSourceId(request.getSourceId());
+		response.setTimestamp(Instant.now().toEpochMilli());
+		System.out.println(String.format("Dry-Test  request %s to endpoint '%s'", request.getId(), request.getEndpoint().getUri()));
+		for (JavaParameter p : request.getParameter()) {
+			System.out.println(String.format("  - Parameters %s with name '%s' and type '%s' - value = '%s'", p.getNumber(), p.getName(), p.getTypeString(), Objects.isNull(p.getValue()) ? "<null>" : p.getValue().toString()));
+		}
+		return response;
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see de.avatar.connector.api.AvatarConnector#executeRequest(de.avatar.model.connector.EndpointRequest)
+	 */
+	@Override
+	public EndpointResponse executeRequest(EndpointRequest request) {
+		EndpointResponse response = connectorFactory.createEndpointResponse();
+		response.setId(request.getId());
+		response.setRequest(EcoreUtil.copy(request));
+		response.setSourceId(request.getSourceId());
+		response.setTimestamp(Instant.now().toEpochMilli());
+		System.out.println(String.format("Execute request %s to endpoint '%s'", request.getId(), request.getEndpoint().getUri()));
+		for (JavaParameter p : request.getParameter()) {
+			System.out.println(String.format("  - Parameters %s with name '%s' and type '%s' - value = '%s'", p.getNumber(), p.getName(), p.getTypeString(), Objects.isNull(p.getValue()) ? "<null>" : p.getValue().toString()));
+		}
+		return response;
+	}
+
+
+}
--- de.avatar.connectors.demo.rsa.config/README.md
+++
@@ -1,4 +0,0 @@
-# `org.gecko.playground.rsa.config`
-
-This project is a multi bundle project and contains the configuration for provider and consumer. Each configuration ends up in an own JAR.
-
