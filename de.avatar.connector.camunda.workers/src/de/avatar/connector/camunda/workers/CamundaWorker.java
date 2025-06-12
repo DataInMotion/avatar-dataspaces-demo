09587c55a73c392b1596314e66b5c7405da46542
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-06-12 14:36:15 +0200

    adjusted target reference for cancel request
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- de.avatar.connector.camunda.workers/src/de/avatar/connector/camunda/workers/CamundaWorker.java
+++ de.avatar.connector.camunda.workers/src/de/avatar/connector/camunda/workers/CamundaWorker.java
@@ -59,7 +59,7 @@ public class CamundaWorker  implements OrchestratorWorker {
 	@Reference(target="(task.status.type=PUBLIC_LINK_REQUEST)")
 	OrchestratorTaskCacheService linkCacheTaskService;
 	
-	@Reference(target="(task.status.type=PROCESS_CANCEL_REQUEST)")
+	@Reference(target="(task.status.type=CANCEL_REQUEST)")
 	OrchestratorTaskCacheService cancelCacheTaskService;
 	
 	
