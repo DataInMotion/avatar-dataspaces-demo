77502eaf4106a42aa89371d7150dc6eb22cfcb3b
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2025-08-21 11:31:44 +0200

    making StatusService implementing CronJob
    status cleanup made daily instead of hourly
    
    added status cleanup for cache with auth
    
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- de.avatar.query.backend.impl/src/de/avatar/query/backend/impl/StatusServiceImpl.java
+++ de.avatar.query.backend.impl/src/de/avatar/query/backend/impl/StatusServiceImpl.java
@@ -29,14 +29,15 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.osgi.service.component.ComponentServiceObjects;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import de.avatar.connector.cleanup.api.api.AvatarDataCleanup;
+import biz.aQute.scheduler.api.Constants;
+import biz.aQute.scheduler.api.CronExpression;
+import biz.aQute.scheduler.api.CronJob;
 import de.avatar.connector.cleanup.api.api.AvatarDataCleanupConfig;
 import de.avatar.model.connector.EndpointResponse;
 import de.avatar.query.backend.api.StatusService;
@@ -53,32 +54,33 @@ import de.avatar.status.StatusFactory;
  * @author ilenia
  * @since Jan 13, 2025
  */
-@Component(immediate = true, name = "StatusService", service = {StatusService.class, AvatarDataCleanup.class}, 
-configurationPid = "StatusServiceCleanup", configurationPolicy = ConfigurationPolicy.REQUIRE)
-public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
+@Component(immediate = true, name = "StatusService", service = {StatusService.class})
+@CronExpression(name = "StatusServiceCleanup", cron = { Constants.CRON_EXPRESSION_DAILY,
+		Constants.CRON_EXPRESSION_REBOOT })
+public class StatusServiceImpl implements StatusService, CronJob{
 
 	@Reference
 	private ComponentServiceObjects<ResourceSet> rsFactory;
 
-	
+
 
 	private static final Logger LOGGER = Logger.getLogger(StatusServiceImpl.class.getName());	
 
 	private Map<String, QueryRequest> cachedRequests = new ConcurrentHashMap<>();
 	private Map<String, QueryResponse> cachedStatuses = new ConcurrentHashMap<>();
-	
+
 	private Map<String, Map<String, QueryRequest>> cachedRequestsWithAuth = new ConcurrentHashMap<>();
 	private Map<String, Map<String, QueryResponse>> cachedStatusesWithAuth = new ConcurrentHashMap<>();
-	
+
 	private AvatarDataCleanupConfig cleanupConfig;
 	private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-	
+
 	@Activate
 	public void activate(AvatarDataCleanupConfig cleanupConfig) {
 		this.cleanupConfig = cleanupConfig;	
 		executor.scheduleAtFixedRate(this::run, cleanupConfig.cleanupDelay(), cleanupConfig.cleanupRate(), TimeUnit.valueOf(cleanupConfig.cleanupUnit()));
 	}
-	
+
 	@Deactivate
 	public void deactivate() {
 		executor.shutdown();
@@ -112,7 +114,7 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 			cachedRequests.put(request.getRequestId(), request);
 		}
 	}
-	
+
 	/* 
 	 * (non-Javadoc)
 	 * @see de.avatar.connector.whiteboard.api.StatusService#updateStatus(de.avatar.status.QueryResponse)
@@ -122,7 +124,7 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 		cachedStatuses.put(queryResponse.getRequestId(), queryResponse);
 	}
 
-	
+
 	/* 
 	 * (non-Javadoc)
 	 * @see de.avatar.connector.whiteboard.api.StatusService#getStatusUpdate(java.lang.String)
@@ -132,14 +134,14 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 		return cachedStatuses.getOrDefault(requestId, createErrorResponse(requestId, String.format("No cached status for request %s", requestId)));
 	}
 
-	
+
 	/* 
 	 * (non-Javadoc)
 	 * @see de.avatar.connector.whiteboard.api.StatusService#updateStatus(de.avatar.model.connector.EndpointResponse, de.avatar.status.SingleConnectorQueryStatus, java.lang.String, boolean)
 	 */
 	@Override
 	public void updateStatus(EndpointResponse endpointResponse, SingleConnectorQueryStatus sgConnQueryStatus, String reqType, boolean isPartialUpdate) {
-		
+
 		LOGGER.info(String.format("I am updating the status for request %s", endpointResponse.getSourceId()));
 		String reqId = endpointResponse.getSourceId();
 		if(!cachedStatuses.containsKey(reqId) || !(cachedStatuses.get(reqId) instanceof QueryStatusResponse)) {
@@ -154,39 +156,61 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 			detailedStatus = StatusFactory.eINSTANCE.createDetailedQueryStatus();
 			statusResponse.setDetailedStatus(detailedStatus);
 		} else {
-//			check if a sgConnStatus for the same connector already exists and if so we replace it with the new one
+			//			check if a sgConnStatus for the same connector already exists and if so we replace it with the new one
 			SingleConnectorQueryStatus oldConnStatus = detailedStatus.getSingleConnectorQueryStatus().
-				stream().
-				filter(scqs -> scqs.getConnectorId().equals(sgConnQueryStatus.getConnectorId())).
-				findAny().
-				orElse(null);
+					stream().
+					filter(scqs -> scqs.getConnectorId().equals(sgConnQueryStatus.getConnectorId())).
+					findAny().
+					orElse(null);
 			if(oldConnStatus != null) {
 				detailedStatus.getSingleConnectorQueryStatus().remove(oldConnStatus);
 			}
 		}
 		detailedStatus.getSingleConnectorQueryStatus().add(sgConnQueryStatus);
-		
+
 		if(isPartialUpdate) statusResponse.setStatus(QueryStatusType.QUERY_PENDING);
 		else if(reqType.equals("dryrun")) statusResponse.setStatus(QueryStatusType.QUERY_DRYRUN_COMPLETED);
 		else statusResponse.setStatus(QueryStatusType.QUERY_COMPLETED);
 	}
-	
+
 	/* 
 	 * (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-		LOGGER.info(String.format("Starting StatusCleanup job! Initial Status Map Size %d", cachedStatuses.size()));
+		LOGGER.info(String.format("Starting StatusCleanup job! Initial Status-Request Map Size %d %d", cachedStatuses.size(), cachedRequests.size()));
 		Instant now = Instant.now();
 		Instant criticInstant = now.minus(cleanupConfig.removeOlderThan(), ChronoUnit.valueOf(cleanupConfig.removeOlderThanUnit()));
 		cachedStatuses.
-			entrySet(). 
-            removeIf(entry -> criticInstant.isAfter(Instant.ofEpochMilli(entry.getValue().getTimestamp()))); 
+		entrySet(). 
+		removeIf(entry -> criticInstant.isAfter(Instant.ofEpochMilli(entry.getValue().getTimestamp()))); 
 		cachedRequests.
-			entrySet().
-			removeIf(entry -> !cachedStatuses.containsKey(entry.getKey()));		
-		LOGGER.info(String.format("Finished StatusCleanup job! Final Status Map Size %d", cachedStatuses.size()));
+		entrySet().
+		removeIf(entry -> !cachedStatuses.containsKey(entry.getKey()));		
+
+		cachedStatusesWithAuth.
+		values().
+		forEach(v -> {
+			v.entrySet().removeIf(entry -> criticInstant.isAfter(Instant.ofEpochMilli(entry.getValue().getTimestamp()))); 
+		});
+		cachedStatusesWithAuth.
+		entrySet().
+		removeIf(entry -> entry.getValue().isEmpty());
+
+		cachedRequestsWithAuth.entrySet().removeIf(entry -> !cachedStatusesWithAuth.containsKey(entry.getKey()));
+		cachedRequestsWithAuth.entrySet().forEach(
+				entry -> {
+					if(cachedStatusesWithAuth.containsKey(entry.getKey())) {
+						entry.getValue().entrySet().removeIf(ev -> !cachedStatusesWithAuth.get(entry.getKey()).containsKey(ev.getKey()));
+					}
+				}
+				);
+
+		cachedRequestsWithAuth.
+		entrySet().
+		removeIf(entry -> entry.getValue().isEmpty());
+		LOGGER.info(String.format("Finished StatusCleanup job! Initial Status-Request Map Size %d %d", cachedStatuses.size(), cachedRequests.size()));
 	}
 
 	/* 
@@ -259,7 +283,7 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 		LOGGER.info("Cached request for user are " + cachedRequestsWithAuth.get(userId).size());
 		return cachedRequestsWithAuth.get(userId).keySet().stream().toList();
 	}
-	
+
 	/* 
 	 * (non-Javadoc)
 	 * @see de.avatar.query.backend.api.StatusService#updateStatus(de.avatar.status.QueryResponse, java.lang.String)
@@ -287,7 +311,7 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 			LOGGER.severe(String.format("Cannot update status for request %s for user because userId is not found in token", endpointResponse.getSourceId()));
 			return;
 		}
-		
+
 		LOGGER.info(String.format("I am updating the status for user for request %s", endpointResponse.getSourceId()));
 		String reqId = endpointResponse.getSourceId();
 		if(!cachedStatusesWithAuth.containsKey(userId)) cachedStatusesWithAuth.put(userId, new HashMap<>());
@@ -303,31 +327,31 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 			detailedStatus = StatusFactory.eINSTANCE.createDetailedQueryStatus();
 			statusResponse.setDetailedStatus(detailedStatus);
 		} else {
-//			check if a sgConnStatus for the same connector already exists and if so we replace it with the new one
+			//			check if a sgConnStatus for the same connector already exists and if so we replace it with the new one
 			SingleConnectorQueryStatus oldConnStatus = detailedStatus.getSingleConnectorQueryStatus().
-				stream().
-				filter(scqs -> scqs.getConnectorId().equals(sgConnQueryStatus.getConnectorId())).
-				findAny().
-				orElse(null);
+					stream().
+					filter(scqs -> scqs.getConnectorId().equals(sgConnQueryStatus.getConnectorId())).
+					findAny().
+					orElse(null);
 			if(oldConnStatus != null) {
 				detailedStatus.getSingleConnectorQueryStatus().remove(oldConnStatus);
 			}
 		}
 		detailedStatus.getSingleConnectorQueryStatus().add(sgConnQueryStatus);
-		
+
 		if(isPartialUpdate) statusResponse.setStatus(QueryStatusType.QUERY_PENDING);
 		else if(reqType.equals("dryrun")) statusResponse.setStatus(QueryStatusType.QUERY_DRYRUN_COMPLETED);
 		else statusResponse.setStatus(QueryStatusType.QUERY_COMPLETED);
-		
+
 	}
-	
+
 	private String extractUserIdFromToken(String token) {
 		DecodedJWT decodedJWT = JWT.decode(token);
 		// Get claims from the payload
-	    String userId = decodedJWT.getClaim("sub").asString();
-	    return userId;
+		String userId = decodedJWT.getClaim("sub").asString();
+		return userId;
 	}
-	
+
 	private QueryResponse createErrorResponse(String requestId, String msg) {
 		QueryStatusResponse response = StatusFactory.eINSTANCE.createQueryStatusResponse();
 		response.setRequestId(requestId);
@@ -336,7 +360,7 @@ public class StatusServiceImpl implements StatusService, AvatarDataCleanup{
 		return response;
 	}
 
-	
+
 }
 
 
